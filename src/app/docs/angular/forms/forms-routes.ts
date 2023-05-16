68a1ee7699867076b1056caf924ec223febe734b
Manthan Ankolekar <manthan.ank@gmail.com>
2023-05-16 19:39:41 +0530

    feat: removed app

--- src/app/docs/angular/forms/forms-routes.ts
+++
@@ -1,8 +0,0 @@
-import { Route } from "@angular/router";
-import { FormsComponent } from "./forms.component";
-
-export const FORMS_ROUTES: Route[] = [
-    {
-        path: '', component: FormsComponent,
-    }
-]
--- src/app/docs/angular/life-cycle-hooks/life-cycle-hooks-routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from "@angular/router";
-import { LifeCycleHooksComponent } from "./life-cycle-hooks.component";
-
-
-export const LIFECYCLEHOOKS_ROUTES: Route[] = [
-    {
-        path: '', component: LifeCycleHooksComponent,
-    }
-]
