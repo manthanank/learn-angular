76a8acf12a58309f2a6c651c83bc7bd600a16a7b
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-08-05 12:50:05 +0530

    chore: Update styles configuration in angular.json and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'learn-angular';
