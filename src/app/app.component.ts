c3a21836c34bc4a45c4f7c928db7c6c95fc41d9f
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-01-29 23:31:19 +0530

    chore: Update Angular dependencies to version 19 and adjust app.component.ts formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'learn-angular';
