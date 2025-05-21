9053c54c59f336741d5b2d55853a54731321744b
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-05-21 09:06:14 +0530

    chore: update package version and dependencies
    - Changed package version from 2.5.0 to 0.0.0.
    - Updated Angular dependencies to version 19.2.0.
    - Updated Angular CLI and build tools to version 19.2.12.
    - Updated TypeScript version to 5.7.2.
    - Updated jasmine-core version to 5.6.0.
    - Removed unused app.component.scss file.
    - Changed app.component.ts to use app.component.css instead of app.component.scss.
    - Updated index.html title and removed unnecessary meta tags.
    - Removed styles.scss file and created styles.css for global styles.
    - Adjusted tsconfig.json to remove sourceMap and declaration options.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'learn-angular';
